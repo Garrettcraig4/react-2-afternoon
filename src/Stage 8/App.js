04dfab70d223fa8b85d9b4fa0bb14626d923e14c
Garrett <Garrettcrg@gmail.com>
2018-01-29 18:15:38 -0600

    Finished

--- src/Stage 8/App.js
+++ src/Stage 8/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-
-import Employee from './models/Employee';
+import React, { Component } from "react";
+import Header, { Component } from "./components/Header";
+import EmployeeList, { Component } from "./components/EmployeeList";
+import EmployeeEditor, { Component } from "./components/EmployeeEditor";
+import Employee from "./models/Employee";
 
 // components
 
@@ -14,13 +16,19 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        // render Header here
+        <Header />
         <div className="main-container">
-          // render EmployeeList here
-          // render EmployeeEditor
+          <EmployeeLis
+            employees={this.state.employees}
+            selectEmployee={this.selectEmployee.bind(this)}
+          />
+          <EmployeeEditor
+            selected={this.state.selectedEmployee}
+            refreshList={this.refresh.bind(this)}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
